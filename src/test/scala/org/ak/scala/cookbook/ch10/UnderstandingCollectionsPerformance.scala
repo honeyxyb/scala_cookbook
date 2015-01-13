75185a43afe93c610c4547b91f1402957133edf4
Anton Korovin <korovin.anton@gmail.com>
2015-01-13 12:07:31 +0200

    Added test class for UnderstandingCollectionsPerformance.

---
+++ src/test/scala/org/ak/scala/cookbook/ch10/UnderstandingCollectionsPerformance.scala
@@ -0,0 +1,19 @@
+package org.ak.scala.cookbook.ch10
+
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+ * @author antonk
+ * @since  12/29/14 - 12:58 PM
+ */
+class UnderstandingCollectionsPerformance
+  extends FunSuite
+          with Matchers {
+
+  // <editor-fold desc="Performance characteristics for sequential collections">
+
+
+  // </editor-fold>
+
+
+}
